fd08cc2bff367b4958a1e95a398980c3f8eb97aa
Ilya <ilya5946096@gmail.com>
2023-01-27 03:12:01 +0300

    Task06

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -3,3 +3,20 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+
+
+
+Console.WriteLine("Введите число 1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число {num} делится на 2?  ");
+
+
+if (num%2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
